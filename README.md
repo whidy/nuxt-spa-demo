71db62e13ab5a864ed2286b4c5a056cd961a9329
Whidy at Home <whidy@whidy.net>
2018-06-17 22:53:24 +0800

    nuxt starter blank

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # nuxt-spa-demo
-based on nuxt, lot of examples in different branch, for a note
+
+> examples of nuxt spa mode
+
+本仓用于在学习Nuxt中使用SPA模式下开发的相关总结
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <div class="demo-wrap">
+      <app-logo/>
+      <h1 class="title">
+        nuxt-spa-demo
+      </h1>
+      <h2 class="subtitle">
+        examples of nuxt spa mode
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.demo-wrap {
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+}
+</style>
