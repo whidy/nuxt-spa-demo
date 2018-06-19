9b34a3e2d7cd36387b3e20e3b7681c21cb6abf47
Whidy at Home <whidy@whidy.net>
2018-06-19 22:44:09 +0800

    ing

---
+++ nuxt.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  mode: 'spa',
+  head: {
+    title: 'nuxt-spa-demo',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'examples of nuxt spa mode' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: ['~/assets/styles/index.scss'],
+  loading: { color: '#3B8070' },
+  build: {
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="container">
+    <div class="demo-wrap">
+      <app-logo/>
+      <h1 class="title">
+        nuxt-spa-demo
+      </h1>
+      <h2 class="subtitle">
+        Nuxt with sessionStorage or cookie
+      </h2>
+      <div class="content">
+        <p>{{ counter }}</p>
+        <p>
+          <button @click="increment">+</button>
+          <button @click="decrement">-</button>
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import { mapState, mapMutations } from 'vuex'
+export default {
+  components: {
+    AppLogo
+  },
+  computed: {
+    ...mapState(['counter'])
+  },
+  methods: {
+    ...mapMutations(['increment', 'decrement'])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.demo-wrap {
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system,
+      BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+      sans-serif;
+    display: block;
+    font-weight: 300;
+    font-size: 28px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 20px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+}
+.content {
+  button {
+    padding: 10px;
+    margin: 10px;
+  }
+}
+</style>
